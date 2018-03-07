aea1a2b930d9dcc83284fcbf1b522309d5644c68
Neusoft <Neusoft@192.168.52.1>
2018-03-07 16:14:35 +0800

    初始化

---
+++ src/main/java/com/algea/web/AlgeaWebApplication.java
@@ -0,0 +1,12 @@
+package com.algea.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgeaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgeaWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/algea/web/AlgeaWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.algea.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgeaWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
